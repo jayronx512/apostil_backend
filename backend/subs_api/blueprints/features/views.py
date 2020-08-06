1a64404ad5d5aaccae92966849a84b6b3ef13fe7
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-08-06 20:26:28 +0800

    deploy from a copy

---
+++ backend/subs_api/blueprints/features/views.py
@@ -0,0 +1,336 @@
+from flask import Blueprint, request, json, jsonify
+from models.user import User
+from models.subscription import Subscription
+from werkzeug.security import check_password_hash
+from datetime import date
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+import calendar
+import datetime
+from datetime import date, timedelta
+
+features_api_blueprint = Blueprint('features_api',
+                             __name__,
+                             template_folder='templates')
+
+def convert_date(react_date):
+    year = int(react_date[0:4])
+    month = int(react_date[5:7])
+    day = int(react_date[8:10])
+    return date(year,month,day)
+
+def add_days(sourcedate, days):
+    delta = timedelta(days)
+    return sourcedate + delta
+
+def add_weeks(sourcedate, weeks):
+    delta = timedelta(7*weeks)
+    return sourcedate + delta
+
+def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = sourcedate.year + month // 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
+    return datetime.date(year, month, day)
+
+def add_years(sourcedate, years):
+    try:
+        return sourcedate.replace(year = sourcedate.year + years)
+    except ValueError:
+        return sourcedate + (date(sourcedate.year + years, 3 , 1) - date(sourcedate.year, 3,1))
+
+@features_api_blueprint.route('/', methods = ['POST'])
+@jwt_required
+def add():
+    user_id = get_jwt_identity()
+    name = request.json.get('name')
+    amount = request.json.get('amount')
+    payment_date = request.json.get('payment_date')
+    payment_date = convert_date(payment_date)
+    subs_type = request.json.get('subs_type')
+    frequency = request.json.get('frequency')
+    description = request.json.get('description')
+    new_subs = Subscription(user_id=user_id, name=name, amount=amount, payment_date=payment_date, subs_type=subs_type, frequency = int(frequency), description=description)
+
+    if new_subs.save():
+        responseObj = {
+            'status': 'success',
+            'message': 'New subscription added!',
+            'subscription': {   'name': name,
+                                'amount': amount,
+                                'next_payment': new_subs.next_payment,
+                                'description': description}
+        }
+
+        return jsonify(responseObj), 200
+
+    else: 
+        responseObj = {
+            'status': 'failed',
+            'message': 'Subscription has failed to be added'
+        }
+
+        return jsonify(responseObj), 200
+
+
+@features_api_blueprint.route('/subscriptions', methods = ['GET'])
+@jwt_required
+def subscription():
+    user_id = get_jwt_identity()
+    current_user = User.get_by_id(user_id)
+    subs_obj = Subscription.select().where(Subscription.user_id == current_user.id).order_by(Subscription.id.asc())
+    subs_arr = []
+    monthly_amount = 0
+    if subs_obj:
+        for sub in subs_obj: 
+            str_amount = ""
+            if sub.amount == 0:
+                str_amount = None
+            elif sub.subs_type == "yearly" and sub.amount is not None:
+                str_amount = "RM" + str(sub.amount) + "/y"
+                monthly_amount = monthly_amount + (sub.amount/12)
+            elif sub.subs_type == "monthly" and sub.amount is not None:
+                str_amount = "RM" + str(sub.amount) + "/m"
+                monthly_amount = monthly_amount + sub.amount
+            elif sub.subs_type == "weekly" and sub.amount is not None:
+                str_amount = "RM" + str(sub.amount) + "/w"
+                monthly_amount = monthly_amount + (sub.amount*4)
+            elif sub.subs_type == "daily" and sub.amount is not None:
+                str_amount = "RM" + str(sub.amount) + "/d"
+                monthly_amount = monthly_amount + (sub.amount*30)
+
+            temp_date = sub.next_payment
+            if (sub.next_payment - date.today()).days <= 0 and sub.paid == True:
+                if sub.subs_type == "daily":
+                    new_next_payment = add_days(temp_date, int(sub.frequency))
+                    query = Subscription.update(payment_date = temp_date, next_payment = new_next_payment, paid = False, due = False).where(Subscription.id == sub.id) 
+                    query.execute()
+                
+                elif sub.subs_type == "weekly":
+                    new_next_payment = add_weeks(temp_date, int(sub.frequency))
+                    query = Subscription.update(payment_date = temp_date, next_payment = new_next_payment, paid = False, due = False).where(Subscription.id == sub.id) 
+                    query.execute()
+
+                elif sub.subs_type == "monthly":
+                    new_next_payment = add_months(temp_date, int(sub.frequency))
+                    query = Subscription.update(payment_date = temp_date, next_payment = new_next_payment, paid = False, due = False).where(Subscription.id == sub.id) 
+                    query.execute()
+
+                elif sub.subs_type == "yearly":
+                    new_next_payment = add_years(temp_date, int(sub.frequency))
+                    query = Subscription.update(payment_date = temp_date, next_payment = new_next_payment, paid = False, due = False).where(Subscription.id == sub.id) 
+                    query.execute()
+
+            elif (sub.next_payment - date.today()).days <= 0 and sub.paid == False:
+                query = Subscription.update(due = True).where(Subscription.id == sub.id)
+                query.execute() 
+
+            subs_list = {
+                'id': sub.id,
+                'name': sub.name,
+                'amount': sub.amount,
+                'str_amount': str_amount,
+                'subs_type': sub.subs_type,
+                'last_payment': sub.payment_date.strftime('%A %d %b %Y'),
+                'next_payment': sub.next_payment.strftime('%A %d %b %Y'),
+                'description': sub.description,
+                'paid': sub.paid,
+                'due': sub.due
+            }
+
+            subs_arr.append(subs_list)
+        
+        responseObj = {
+            'status': 'success',
+            'subscriptions': subs_arr,
+            'monthly_amount': monthly_amount,
+            'username': current_user.username
+        }
+
+        return jsonify(responseObj), 200
+    
+    else:
+        responseObj = {
+            'status': 'success but array is empty',
+            'subscriptions': subs_arr,
+            'monthly_amount': monthly_amount,
+            'username': current_user.username
+        }
+
+        return jsonify(responseObj), 200
+
+@features_api_blueprint.route('/subs_delete/<id>', methods=['POST'])
+@jwt_required 
+def subs_delete(id):
+    user_id = get_jwt_identity()
+    sub = Subscription.get_by_id(id)
+    if sub.delete_instance():
+        subs_obj = Subscription.select().where(Subscription.user_id == user_id)
+        subs_arr = []
+        if subs_obj:
+            for sub in subs_obj:
+                str_amount = ""
+                if sub.subs_type == "yearly" and sub.amount is not None:
+                    str_amount = "RM" + str(sub.amount) + "/y"
+                elif sub.subs_type == "monthly" and sub.amount is not None:
+                    str_amount = "RM" + str(sub.amount) + "/m"
+                elif sub.subs_type == "weekly" and sub.amount is not None:
+                    str_amount = "RM" + str(sub.amount) + "/w"
+                elif sub.subs_type == "daily" and sub.amount is not None:
+                    str_amount = "RM" + str(sub.amount) + "/d"
+
+                subs_list = {
+                    'id': sub.id,
+                    'name': sub.name,
+                    'amount': sub.amount,
+                    'str_amount': str_amount,
+                    'subs_type': sub.subs_type,
+                    'last_payment': sub.payment_date.strftime('%A %d %b %Y'),
+                    'next_payment': sub.next_payment.strftime('%A %d %b %Y'),
+                    'description': sub.description,
+                    'paid': sub.paid,
+                    'due': sub.due
+                }
+
+                subs_arr.append(subs_list)
+
+        responseObj = {
+            'status': 'success',
+            'message': 'Successfully deleted subscription!',
+            'subscriptions': subs_arr
+        }
+
+        return jsonify(responseObj), 200
+    
+    else:
+        responseObj = {
+            'status': 'failed',
+            'message': 'Failed to delete your subscription'
+        }
+
+        return jsonify(responseObj), 400
+
+@features_api_blueprint.route('/status/<id>', methods = ['POST'])
+@jwt_required
+def status(id): 
+    user_id = get_jwt_identity()
+    sub = Subscription.get_by_id(id)
+    query = Subscription.update(paid = not sub.paid).where(Subscription.id == id)
+
+    if query.execute():
+        subs_obj = Subscription.select().where(Subscription.user_id == user_id).order_by(Subscription.id.asc())
+        subs_arr = []
+        if subs_obj:
+            for sub in subs_obj:
+                str_amount = ""
+                if sub.subs_type == "yearly" and sub.amount is not None:
+                    str_amount = "RM" + str(sub.amount) + "/y"
+                elif sub.subs_type == "monthly" and sub.amount is not None:
+                    str_amount = "RM" + str(sub.amount) + "/m"
+                elif sub.subs_type == "weekly" and sub.amount is not None:
+                    str_amount = "RM" + str(sub.amount) + "/w"
+                elif sub.subs_type == "daily" and sub.amount is not None:
+                    str_amount = "RM" + str(sub.amount) + "/d"
+
+                subs_list = {
+                    'id': sub.id,
+                    'name': sub.name,
+                    'amount': sub.amount,
+                    'str_amount': str_amount,
+                    'subs_type': sub.subs_type,
+                    'last_payment': sub.payment_date.strftime('%A %d %b %Y'),
+                    'next_payment': sub.next_payment.strftime('%A %d %b %Y'),
+                    'description': sub.description,
+                    'paid': sub.paid,
+                    'due': sub.due
+                }
+
+                subs_arr.append(subs_list)
+
+        responseObj = {
+            'status': 'success',
+            'subscriptions': subs_arr
+        }
+
+        return jsonify(responseObj), 200
+    
+    else:
+        responseObj = {
+            'status': 'failed',
+            'message': 'Failed to change your paid status'
+        }
+
+        return jsonify(responseObj), 400
+
+@features_api_blueprint.route('/sub_data/<id>', methods=['GET'])
+@jwt_required
+def get_sub_data(id):
+    user_id = get_jwt_identity()
+    sub = Subscription.get_by_id(id)
+    subs_list = {
+        'id': sub.id,
+        'name': sub.name,
+        'amount': sub.amount,
+        'description': sub.description,
+        'frequency': sub.frequency,
+        'subs_type': sub.subs_type,
+        'payment_date': sub.payment_date,
+        
+    }
+    
+    responseObj = {
+        'status': 'success',
+        'subscriptions': subs_list
+    }
+
+    return jsonify(responseObj), 200
+
+@features_api_blueprint.route('/edit/<id>', methods=['POST'])
+@jwt_required
+def edit(id): 
+    user_id = get_jwt_identity()
+    sub = Subscription.get_by_id(id)
+    name = request.json.get('name')
+    amount = request.json.get('amount')
+    description = request.json.get('description')
+    frequency = request.json.get('frequency')
+    frequency = int(frequency)
+    subs_type = request.json.get('subs_type')
+    payment_date = request.json.get('payment_date')
+    payment_date = convert_date(payment_date)
+
+    if subs_type == "daily": 
+        next_payment = add_days(payment_date, frequency)
+
+    elif subs_type == "weekly":
+        next_payment = add_weeks(payment_date, frequency)
+
+    elif subs_type == "monthly":
+        next_payment = add_months(payment_date, frequency)
+
+    elif subs_type == "yearly":
+        next_payment = add_years(payment_date, frequency)
+
+    query = Subscription.update(name=name, amount=amount, description=description, frequency=frequency, subs_type=subs_type, payment_date=payment_date, next_payment=next_payment).where(Subscription.id == sub.id)
+    
+    if query.execute():
+        responseObj = {
+            'status': 'success',
+            'message': 'Successfully edited subscription'
+        }
+
+        return jsonify(responseObj), 200
+    
+    else:
+        responseObj = {
+            'status': 'failed',
+            'message': 'Failed to edit your subscription'
+        }
+
+        return jsonify(responseObj), 400
+
+
+
+
+
+
---
+++ backend/subs_api/blueprints/users/views.py
@@ -0,0 +1,85 @@
+from flask import Blueprint, request, json, jsonify
+from models.user import User
+from werkzeug.security import check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
+users_api_blueprint = Blueprint('users_api',
+                             __name__,
+                             template_folder='templates')
+
+@users_api_blueprint.route('/', methods=['GET'])
+def index():
+    breakpoint()
+    return "USERS API"
+
+@users_api_blueprint.route('/signup', methods = ['POST'])
+def add(): 
+    username = request.json.get('username')
+    first_name = request.json.get('first_name')
+    last_name = request.json.get('last_name')
+    email = request.json.get('email')
+    password = request.json.get('password')
+    hp_number = request.json.get('hp_number')
+
+    new_user = User(username=username, email=email, first_name=first_name, last_name=last_name, password=password, hp_number=hp_number)
+
+    if new_user.save():
+        responseObj = {
+            'status': 'success',
+            'message': 'New user successfully added',
+            'user': {   'id': int(new_user.id), 
+                        'username': new_user.username, 
+                        'email': new_user.email, 
+                        'first_name': new_user.first_name,
+                        'last_name': new_user.last_name,
+                        'hp_number': new_user.hp_number}
+        }
+
+        return jsonify(responseObj), 200
+
+    else:
+        responseObj = {
+            'status': 'failed',
+            'message': new_user.errors
+        }
+
+        return jsonify(responseObj), 400
+
+@users_api_blueprint.route('/login', methods = ['POST'])
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    login_user = User.get_or_none(username=username)
+
+    if login_user:
+        if check_password_hash(login_user.password, password):
+            access_token = create_access_token(identity = login_user.id, expires_delta = False)
+            responseObj = {
+                'status': 'success',
+                'message': 'Login successfully',
+                'token': access_token,
+                'user': {   "id": int(login_user.id),
+                            "username": login_user.username,
+                            "email": login_user.email,
+                            "first_name": login_user.first_name,
+                            "last_name": login_user.last_name,
+                            "hp_number": login_user.hp_number,
+                            }
+            } 
+            return jsonify(responseObj), 200
+
+        else: 
+            responseObj = {
+                'status': 'failed',
+                'message': 'Incorrect Password!'
+            } 
+            return jsonify(responseObj), 400
+    
+    else: 
+        responseObj = {
+            'status': 'failed',
+            'message': 'Incorrect username!'
+        }  
+        return jsonify(responseObj), 400
+
+
